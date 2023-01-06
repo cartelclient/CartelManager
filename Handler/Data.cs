d33bab830d2d3c05e4f610b21392285b08a09904
romance <66000596+romancewastaken@users.noreply.github.com>
2023-01-06 11:47:37 -0500

    Create Data.cs

---
+++ Handler/Data.cs
@@ -0,0 +1,12 @@
+namespace CartelManager.Handler
+{
+    internal class Data
+    {
+        public static string authKey = "";
+        public static string aid = "";
+        public static string secret = "";
+        public static string apiKey = "";
+
+        public static string version = "1.0";
+    }
+}
