f7d95a8af9ad8890c8f4ade8966e5bcdce4a4c3e
romance <66000596+romancewastaken@users.noreply.github.com>
2023-01-12 18:10:52 -0500

    Add files via upload

---
+++ Handler/webhookSender.cs
@@ -0,0 +1,41 @@
+﻿using Authenticator;
+using System;
+using System.Collections.Specialized;
+using System.Net;
+using System.Windows.Forms;
+
+namespace CartelManager.Handler
+{
+    internal class webhookSender
+    {
+        public static void Send(string url, string username, string content)
+        {
+            try
+            {
+                WebClient wc = new WebClient();
+                wc.UploadValues(url, new NameValueCollection
+                {
+                {
+                    "content", content
+                },
+                {
+                    "username", username
+                }
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.Clear();
+                Console.WriteLine("There was an error sending your webhook login info, therefore the app has to exit. Send your error message in a staff channel (The error is automatically copied to your clipboard)");
+                Clipboard.SetText(ex.Message);
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+        }
+
+        public static void authLog(string action)
+        {
+            Auth.Log(User.Username, action);
+        }
+    }
+}
