7f8e6f8dd99ba2fddeed19c16aeb8193bbb322f5
romance <66000596+romancewastaken@users.noreply.github.com>
2023-01-26 17:47:21 -0500

    Update Data.cs

--- Handler/Data.cs
+++ Handler/Data.cs
@@ -1,12 +1,18 @@
+using System.Security.Principal;
+
 namespace CartelManager.Handler
 {
     internal class Data
     {
+        public static string webhook = "";
         public static string authKey = "";
         public static string aid = "";
         public static string secret = "";
+        public static string programSecret = "";
         public static string apiKey = "";
 
         public static string version = "1.0";
+
+        public static string HwID = WindowsIdentity.GetCurrent().User.Value.ToString();
     }
 }
